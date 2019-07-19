f4333f611bfe51aa8a4c4ea9468f5d0094c2b9b4
Sandip <sandiproy10@yahoo.com>
2019-07-19 20:41:21 +0800

    Update Day 1: data types.js
    added some comments

--- Day 1: data types.js
+++ Day 1: data types.js
@@ -1,4 +1,4 @@
-Task 
+/* Task 
 Complete the code in the editor below. The variables , , and  are already declared and initialized for you. You must:
 
 Declare  variables: one of type int, one of type double, and one of type String.
@@ -18,7 +18,7 @@ The third line contains a string that you must concatenate with .
 Output Format
 
 Print the sum of both integers on the first line, the sum of both doubles (scaled to  decimal place) on the second line, and then the two concatenated strings on the third line.
-
+*/
 
 //***************** solution **************************
 
