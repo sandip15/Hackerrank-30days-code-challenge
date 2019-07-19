bb1c23b97cf7704318ebd78b5c3a538073132b28
Sandip <sandiproy10@yahoo.com>
2019-07-19 20:43:50 +0800

    Create Day 2: operators.js

---
+++ Day 2: operators.js
@@ -0,0 +1,60 @@
+/* Task 
+Given the meal price (base cost of a meal), tip percent (the percentage of the meal price being added as tip), and tax percent (the percentage of the meal price being added as tax) for a meal, find and print the meal's total cost.
+
+Note: Be sure to use precise values for your calculations, or you may end up with an incorrectly rounded result!
+
+Input Format
+
+There are  lines of numeric input: 
+The first line has a double,  (the cost of the meal before tax and tip). 
+The second line has an integer,  (the percentage of  being added as tip). 
+The third line has an integer,  (the percentage of  being added as tax).
+
+Output Format
+
+Print the total meal cost, where  is the rounded integer result of the entire bill ( with added tax and tip). 
+
+*/
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the solve function below.
+function solve(meal_cost, tip_percent, tax_percent) {
+    var tip = meal_cost * (tip_percent / 100)
+    var tax = meal_cost * (tax_percent / 100)
+
+    var total = Math.round(meal_cost + tip + tax)
+    console.log(total)
+}
+
+function main() {
+    const meal_cost = parseFloat(readLine());
+
+    const tip_percent = parseInt(readLine(), 10);
+
+    const tax_percent = parseInt(readLine(), 10);
+
+    solve(meal_cost, tip_percent, tax_percent);
+}
