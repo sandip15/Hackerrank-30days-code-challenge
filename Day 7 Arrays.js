43201b4525603020dec74706a4025304af420ae2
Sandip <sandiproy10@yahoo.com>
2019-07-26 17:07:42 +0800

    Update Day 7 Arrays.js

--- Day 7 Arrays.js
+++ Day 7 Arrays.js
@@ -1,11 +1,11 @@
 /*
-Given an array, A ,N of  integers, print A's elements in reverse order as a single line of space-separated numbers.
+Given an array,A,N of integers, print A's elements in reverse order as a single line of space-separated numbers.
 
-Input Format
+Input Format: 
 The first line contains an integer, N  (the size of our array). 
 The second line contains N space-separated integers describing array A's elements.
 
-Output Format
+Output Format:
 Print the elements of array  in reverse order as a single line of space-separated numbers.
 
 sample input:
