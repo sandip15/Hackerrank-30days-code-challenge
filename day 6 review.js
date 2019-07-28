9531928fb840eb80029a9ac74c8cab6edf0610c0
Sandip <sandiproy10@yahoo.com>
2019-07-28 22:21:02 +0800

    Update day 6 review.js

--- day 6 review.js
+++ day 6 review.js
@@ -1,10 +1,9 @@
 /*
-Given a string,S , of length N that is indexed from 0 to N-1, print its even-indexed and odd-indexed characters as 2 space-separated strings on a single line (see the Sample below for more detail).
+Given a string,S , of length N that is indexed from 0 to N-1, print its even-indexed and odd-indexed characters as 
+2 space-separated strings on a single line (see the Sample below for more detail).
 
 Input Format
-
-The first line contains an integer,T (the number of test cases). 
-Each line i of the T  subsequent lines contain a String, S .
+The first line contains an integer,T (the number of test cases). Each line i of the T  subsequent lines contain a String, S .
 Sample Input:
 2
 Hacker
