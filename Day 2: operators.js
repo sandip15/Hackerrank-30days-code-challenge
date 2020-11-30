6947b4a792e21ab24915acacdd674670b9dddbcc
Sandip Roy <sandiproy10@yahoo.com>
2020-11-30 23:10:18 +0600

    Update Day 2: operators.js

--- Day 2: operators.js
+++ Day 2: operators.js
@@ -4,8 +4,6 @@ and tax percent (the percentage of the meal price being added as tax) for a meal
 
 
 Input Format
-
-There are  lines of numeric input: 
 The first line has a double,  (the cost of the meal before tax and tip). 
 The second line has an integer,  (the percentage of  being added as tip). 
 The third line has an integer,  (the percentage of  being added as tax).
